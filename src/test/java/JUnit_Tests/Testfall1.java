e4603c8180a7451b0f2d71d1375a26de9dffbcf6
Franiano <36850648+Franiano@users.noreply.github.com>
2022-03-21 14:19:07 +0100

    Projekt Struktur::::

---
+++ src/test/java/JUnit_Tests/Testfall1.java
@@ -0,0 +1,22 @@
+package JUnit_Tests;
+
+
+import org.junit.Test;
+
+
+public class Testfall1 {
+
+@Test
+public void  liste_testen(){
+    System.out.print("bin in den erstn Test" +
+            "\n");
+    System.out.print("bin in den erstn Test\n");
+    System.out.print("bin in den erstn Test\n");
+    System.out.print("bin in den erstn Test\n");
+    System.out.print("bin in den erstn Test\n");
+
+}
+
+
+}
+//Test
--- src/test/java/Testfall1.java
+++
@@ -1,3 +0,0 @@
-public class Testfall1 {
-}
-//Test
