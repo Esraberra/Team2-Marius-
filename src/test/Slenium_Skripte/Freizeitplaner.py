000cf199df5c08baffd173da9329d9ccf15eb36f
ahabott <101576395+ahabott@users.noreply.github.com>
2022-03-30 15:42:15 +0200

    Seleniumskript erstellt

--- src/test/Slenium_Skripte/Freizeitplaner.py
+++ src/test/Slenium_Skripte/Freizeitplaner.py
@@ -1,58 +1,32 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from time import sleep
 
 driver = webdriver.Chrome()
 
 driver.get('http://localhost:3131/dropdown-menu-form.jsp') # URL wird mithilfe Live Server generiert
 
-element = driver.find_element_by_id("activity")
+element = driver.find_element(By.ID, "activity")
 element.click()
 sleep(2)
 
-element = driver.find_element_by_xpath('//*[@id="activity"]/option[2]')
+element = driver.find_element(By.XPATH, '//*[@id="activity"]/option[2]')
 element.click()
 sleep(2)
 
-##element = driver.find_element_by_xpath('//*[@id="activity"]/option[3]')
-##element.click()
-##sleep(2)
-##
-##element = driver.find_element_by_xpath('//*[@id="activity"]/option[4]')
-##element.click()
-##sleep(2)
-
-element = driver.find_element_by_id("wetter")
+element = driver.find_element(By.ID, "wetter")
 element.click()
 sleep(2)
 
-element = driver.find_element_by_xpath('//*[@id="wetter"]/option[2]')
+element = driver.find_element(By.XPATH, '//*[@id="wetter"]/option[2]')
 element.click()
 sleep(2)
 
-##element = driver.find_element_by_xpath('//*[@id="wetter"]/option[3]')
-##element.click()
-##sleep(2)
-##
-##element = driver.find_element_by_xpath('//*[@id="wetter"]/option[4]')
-##element.click()
-##sleep(2)
-
-element = driver.find_element_by_id("kosten").send_keys("10")
+element = driver.find_element(By.ID, "kosten").send_keys("10")
 sleep(2)
 
-element = driver.find_element_by_id("submitButton")
+element = driver.find_element(By.ID, "submitButton")
 element.click()
-##
-##sleep(2)
-##
-##driver.maximize_window()
-##sleep(2)
-##driver.refresh()
-##sleep(2)
-##
-##driver.get("http://www.sogeti.com")
-##sleep(2)
-##driver.back()
-##sleep(2)
 
-driver.quit()
+
+## driver.quit()
