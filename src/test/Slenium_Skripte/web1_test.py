36e48cf33ed0a862a038e3b28ad7f0e495ad5a51
ahabott <101576395+ahabott@users.noreply.github.com>
2022-03-30 18:01:43 +0200

    Seleniumskript erstellt

--- src/test/Slenium_Skripte/web1_test.py
+++ src/test/Slenium_Skripte/web1_test.py
@@ -6,7 +6,7 @@ import logging
 
 logging.basicConfig(level=logging.DEBUG)
 
-def test_pytest():
+def test_pytest1():
 
     driver = webdriver.Chrome()
 
@@ -40,6 +40,76 @@ def test_pytest():
     element = driver.find_element(By.ID, "submitButton")
     element.click()
     logging.info("Submited")
+def test_pytest2():
+
+    driver = webdriver.Chrome()
+
+    driver.get('http://localhost:3131/dropdown-menu-form.jsp') # URL wird mithilfe Live Server generiert
+    logging.info("Open autmationpractice")
+
+    element = driver.find_element(By.ID, "activity")
+    element.click()
+    logging.info("Click on dropdowm 1")
+    sleep(2)
+
+    element = driver.find_element(By.XPATH, '//*[@id="activity"]/option[3]')
+    element.click()
+    logging.info("Choice 1")
+    sleep(2)
+
+    element = driver.find_element(By.ID, "wetter")
+    element.click()
+    logging.info("Click on dropdowm 2")
+    sleep(2)
+
+    element = driver.find_element(By.XPATH, '//*[@id="wetter"]/option[3]')
+    element.click()
+    logging.info("Choice 2")
+    sleep(2)
+
+    element = driver.find_element(By.ID, "kosten").send_keys("20")
+    logging.info("Input")
+    sleep(2)
+
+    element = driver.find_element(By.ID, "submitButton")
+    element.click()
+    logging.info("Submited")
+
+def test_pytest3():
+
+    driver = webdriver.Chrome()
+
+    driver.get('http://localhost:3131/dropdown-menu-form.jsp') # URL wird mithilfe Live Server generiert
+    logging.info("Open autmationpractice")
+
+    element = driver.find_element(By.ID, "activity")
+    element.click()
+    logging.info("Click on dropdowm 1")
+    sleep(2)
+
+    element = driver.find_element(By.XPATH, '//*[@id="activity"]/option[4]')
+    element.click()
+    logging.info("Choice 1")
+    sleep(2)
+
+    element = driver.find_element(By.ID, "wetter")
+    element.click()
+    logging.info("Click on dropdowm 2")
+    sleep(2)
+
+    element = driver.find_element(By.XPATH, '//*[@id="wetter"]/option[4]')
+    element.click()
+    logging.info("Choice 2")
+    sleep(2)
+
+    element = driver.find_element(By.ID, "kosten").send_keys("30")
+    logging.info("Input")
+    sleep(2)
+
+    element = driver.find_element(By.ID, "submitButton")
+    element.click()
+    logging.info("Submited")
+
 
 
     ## driver.quit()
