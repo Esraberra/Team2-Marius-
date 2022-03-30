726c80ad6129cc2611258cb563d503173afcfb78
ahabott <101576395+ahabott@users.noreply.github.com>
2022-03-30 22:39:21 +0200

    Seleniumskript erstellt

--- src/test/Slenium_Skripte/web1_test.py
+++ src/test/Slenium_Skripte/web1_test.py
@@ -16,25 +16,34 @@ def test_pytest1():
     element = driver.find_element(By.ID, "activity")
     element.click()
     logging.info("Click on dropdowm 1")
+    sleep(2)
 
     element = driver.find_element(By.XPATH, '//*[@id="activity"]/option[2]')
     element.click()
     logging.info("Choice 1")
+    sleep(2)
 
     element = driver.find_element(By.ID, "wetter")
     element.click()
     logging.info("Click on dropdowm 2")
+    sleep(2)
 
     element = driver.find_element(By.XPATH, '//*[@id="wetter"]/option[2]')
     element.click()
     logging.info("Choice 2")
+    sleep(2)
 
     element = driver.find_element(By.ID, "kosten").send_keys("10")
     logging.info("Input")
+    sleep(2)
 
     element = driver.find_element(By.ID, "submitButton")
     element.click()
     logging.info("Submited")
+
+    element = driver.find_element(By.ID, "output")
+    assert "Theater" == element.textx
+
 def test_pytest2():
 
     driver = webdriver.Chrome()
@@ -45,56 +54,72 @@ def test_pytest2():
     element = driver.find_element(By.ID, "activity")
     element.click()
     logging.info("Click on dropdowm 1")
+    sleep(2)
 
     element = driver.find_element(By.XPATH, '//*[@id="activity"]/option[3]')
     element.click()
     logging.info("Choice 1")
+    sleep(2)
 
     element = driver.find_element(By.ID, "wetter")
     element.click()
     logging.info("Click on dropdowm 2")
+    sleep(2)
 
     element = driver.find_element(By.XPATH, '//*[@id="wetter"]/option[3]')
     element.click()
     logging.info("Choice 2")
+    sleep(2)
 
     element = driver.find_element(By.ID, "kosten").send_keys("20")
     logging.info("Input")
+    sleep(2)
 
     element = driver.find_element(By.ID, "submitButton")
     element.click()
     logging.info("Submited")
+
+    element = driver.find_element(By.ID, "output")
+    assert "Theater" == element.text
 
 def test_pytest3():
 
     driver = webdriver.Chrome()
 
+
     driver.get('http://localhost:3131/dropdown-menu-form.jsp') # URL wird mithilfe Live Server generiert
+    driver.maximize_window()
     logging.info("Open autmationpractice")
 
     element = driver.find_element(By.ID, "activity")
     element.click()
     logging.info("Click on dropdowm 1")
+    sleep(2)
 
     element = driver.find_element(By.XPATH, '//*[@id="activity"]/option[4]')
     element.click()
     logging.info("Choice 1")
+    sleep(2)
 
     element = driver.find_element(By.ID, "wetter")
     element.click()
     logging.info("Click on dropdowm 2")
+    sleep(2)
 
     element = driver.find_element(By.XPATH, '//*[@id="wetter"]/option[4]')
     element.click()
     logging.info("Choice 2")
+    sleep(2)
 
     element = driver.find_element(By.ID, "kosten").send_keys("30")
     logging.info("Input")
+    sleep(2)
 
     element = driver.find_element(By.ID, "submitButton")
     element.click()
     logging.info("Submited")
 
+    element = driver.find_element(By.ID, "output")
+    assert "Theater" == element.text
 
 
-    ## driver.quit()
